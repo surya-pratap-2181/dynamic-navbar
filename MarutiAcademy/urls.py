747c7ea9f6afba797b00b6287549b79c2b0bd7fd
Suryaaa <77917442+surya-pratap-2181@users.noreply.github.com>
2021-08-08 13:46:42 +0530

    Delete MarutiAcademy directory

--- MarutiAcademy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls.conf import include
-urlpatterns = [
-    path('', include('home.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
